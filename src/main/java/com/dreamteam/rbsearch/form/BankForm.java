db70f93e9d26a829fb7140cc568fe73a67e5b605
TomSuworof <rc1995king@gmail.com>
2020-09-19 15:04:18 +0300

    -feature(form): dto created

--- src/main/java/com/dreamteam/rbsearch/form/BankForm.java
+++ src/main/java/com/dreamteam/rbsearch/form/BankForm.java
@@ -12,12 +12,19 @@ public class BankForm {
     private String app;
     private Map<String, Object> formFromModel;
 
-    public BankForm (Map<String, Object> map) {
-        this.formFromModel = map;
-//        this.price = map.get("price").toString();
-//        this.app = map.get("app").toString();
-        for (Map.Entry<String, Object> entry : formFromModel.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
+//    public BankForm (Map<String, Object> map) {
+//        this.formFromModel = map;
+////        this.price = map.get("price").toString();
+////        this.app = map.get("app").toString();
+//        for (Map.Entry<String, Object> entry : formFromModel.entrySet()) {
+//            System.out.println(entry.getKey() + " " + entry.getValue());
+//        }
+//    }
+
+    public BankForm(String price, String app) {
+        this.price = price;
+        this.app = app;
     }
+
+    @Override
 }
