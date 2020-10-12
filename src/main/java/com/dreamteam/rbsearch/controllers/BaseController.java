4b13fab0c0fe0339eb7dbd2d64f16e8797df958b
TomSuworof <rc1995king@gmail.com>
2020-10-12 16:55:58 +0300

    -feature(radio): added radio-chooser

--- src/main/java/com/dreamteam/rbsearch/controllers/BaseController.java
+++ src/main/java/com/dreamteam/rbsearch/controllers/BaseController.java
@@ -3,6 +3,7 @@ package com.dreamteam.rbsearch.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +18,18 @@ public class BaseController {
     @GetMapping("/error")
     public String returnErrorPage() {
         return "../public/error/404";
+    }
+
+    @GetMapping("/search?category={category}")
+    public String returnCriteriaPage(@PathVariable String category) {
+        switch (category) {
+            case "individual":
+                return "criteria/individual";
+            case "legal_entity":
+                return "criteria/legal_entity";
+            case "corporation":
+                return "criteria/corporation";
+        }
+        return returnErrorPage();
     }
 }
