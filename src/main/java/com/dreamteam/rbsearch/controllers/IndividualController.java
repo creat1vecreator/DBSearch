8d763e1ef4321bb01ced1ff215c290ae9fe12c6b
TomSuworof <rc1995king@gmail.com>
2020-10-13 14:37:18 +0300

    -feature(template engine): added support of Thymeleaf

--- src/main/java/com/dreamteam/rbsearch/controllers/IndividualController.java
+++ src/main/java/com/dreamteam/rbsearch/controllers/IndividualController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -22,8 +23,24 @@ public class IndividualController {
 //        return "criteria/individual";
 //    }
 
+//    @RequestMapping(value = "/search", method = RequestMethod.GET, produces = "application/json")
+//    public ResponseEntity<String> getSearch(
+//            @RequestParam(value = "price") String price,
+//            @RequestParam(value = "app", required = false) String app) {
+//        IndividualForm individualForm = new IndividualForm(
+//                price,
+//                app
+//        );
+//        System.out.println(individualForm);
+//        BankEntitiesDTO<IndividualEntity> bankEntitiesDTO = individualService.find(individualForm);
+//        String jsonResponse = new Gson().toJson(bankEntitiesDTO.getBankEntities());
+//        System.out.println(jsonResponse);
+//        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+//    }
+
     @RequestMapping(value = "/search", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<String> getSearch(
+    public String getSearch(
+            Model model,
             @RequestParam(value = "price") String price,
             @RequestParam(value = "app", required = false) String app) {
         IndividualForm individualForm = new IndividualForm(
@@ -32,20 +49,30 @@ public class IndividualController {
         );
         System.out.println(individualForm);
         BankEntitiesDTO<IndividualEntity> bankEntitiesDTO = individualService.find(individualForm);
-        String jsonResponse = new Gson().toJson(bankEntitiesDTO.getBankEntities());
-        System.out.println(jsonResponse);
-        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+        model.addAttribute("banks", bankEntitiesDTO.getBankEntities());
+        return "answer/individual";
     }
 
+//    @GetMapping("/bank/{name}")
+//    public @ResponseBody ResponseEntity<String> findByName(@PathVariable String name, Model model) {
+//        try {
+//            IndividualEntity individualEntity = individualService.findByName(name);
+//            String jsonResponse = new Gson().toJson(individualEntity);
+//            System.out.println(jsonResponse);
+//            return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+//        } catch (RuntimeException runtimeException) {
+//            return new ResponseEntity<>("error 404 - not found", HttpStatus.OK);
+//        }
+//    }
+
     @GetMapping("/bank/{name}")
-    public @ResponseBody ResponseEntity<String> findByName(@PathVariable String name) {
+    public String findByName(@PathVariable String name, Model model) {
         try {
             IndividualEntity individualEntity = individualService.findByName(name);
-            String jsonResponse = new Gson().toJson(individualEntity);
-            System.out.println(jsonResponse);
-            return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+            model.addAttribute("bank", individualEntity);
+            return "single/individual";
         } catch (RuntimeException runtimeException) {
-            return new ResponseEntity<>("error 404 - not found", HttpStatus.OK);
+            return "error";
         }
     }
 
