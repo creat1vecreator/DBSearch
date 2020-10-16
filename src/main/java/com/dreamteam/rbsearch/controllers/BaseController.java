d88f60e7077810bf1582963f7a7c1ffe5eb9c235
TomSuworof <rc1995king@gmail.com>
2020-10-16 19:52:39 +0300

    -feature(legal-entity): added legal-entity support

--- src/main/java/com/dreamteam/rbsearch/controllers/BaseController.java
+++ src/main/java/com/dreamteam/rbsearch/controllers/BaseController.java
@@ -1,7 +1,10 @@
 package com.dreamteam.rbsearch.controllers;
 
+import com.dreamteam.rbsearch.services.IndividualService;
+import com.dreamteam.rbsearch.services.LegalEntityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping()
 public class BaseController {
+    private final IndividualService individualService;
+    private final LegalEntityService legalEntityService;
+
     @GetMapping("/")
     public String returnStartPage() {
         return "start_page";
@@ -21,14 +27,14 @@ public class BaseController {
     }
 
     @GetMapping("/{category}")
-    public String returnCriteriaPage(@PathVariable String category) {
+    public String returnCriteriaPage(@PathVariable String category, Model model) {
         switch (category) {
             case "individual":
+                model.addAttribute("banks", individualService.findAll());
                 return "answer/individual";
             case "legal_entity":
+                model.addAttribute("banks", legalEntityService.findAll());
                 return "answer/legal_entity";
-            case "corporation":
-                return "answer/corporation";
         }
         return returnErrorPage();
     }
